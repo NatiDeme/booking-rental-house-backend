8a1e83b76f18f493ea0ab10388742113eb8dd3c4
unknown <tylher123@gmail.com>
2022-10-25 16:39:42 +0100

    Configure jwt for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+    before_action :set_default_format
+    before_action :authenticate_token!
+
+    private
+
+    def set_default_format
+        request.format = :json
+    end
+
+    def auth_token
+        @auth_token || = request.headers.fetch("Authorization","").split(" ").last
+    end
+   
+    def auauthenticate_token!
+        payload = JsonWebToken.decode auth_token
+        if payload.present?
+            @current_user = User.find(payload["sub"])
+        else
+            render json: {errors: ["Invalid auth token"]}, status: :unauthorized
+        end
+    end
+    
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_024_002_506) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_002506) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'reservations', force: :cascade do |t|
-    t.date 'date'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.bigint 'tours_id'
-    t.index ['tours_id'], name: 'index_reservations_on_tours_id'
-    t.index ['user_id'], name: 'index_reservations_on_user_id'
+  create_table "reservations", force: :cascade do |t|
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "tours_id"
+    t.index ["tours_id"], name: "index_reservations_on_tours_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
-  create_table 'tours', force: :cascade do |t|
-    t.string 'image'
-    t.string 'city'
-    t.string 'description'
-    t.integer 'price'
-    t.time 'duration'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.index ['user_id'], name: 'index_tours_on_user_id'
+  create_table "tours", force: :cascade do |t|
+    t.string "image"
+    t.string "city"
+    t.string "description"
+    t.integer "price"
+    t.time "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tours_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'username'
-    t.string 'role', default: 'user'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "role", default: "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
