643d2a8a92793f49cac8c005a36d5648b453e556
unknown <tylher123@gmail.com>
2022-10-25 08:42:10 +0100

    create end-point to add new tour

--- app/controllers/api/v1/tour_controller.rb
+++ app/controllers/api/v1/tour_controller.rb
@@ -3,4 +3,16 @@ class Api::V1::TourController < ApplicationController
     @tour = Tour.all
     render json: @tour
   end
+
+  def create 
+    @newTour = Tour.new(tour_params)
+    if @newTour.save
+      render json: @newTour, status: :created
+    else
+      render json: @newTour.error, status: :unprocessable_entity
+  end
+
+  private
+  def tour_params
+    params.require(:tour).permit(:image,:city,:description,:price,:duration)
 end
