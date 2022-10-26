5670858670a3048028fe43bce663718d4c772c28
NatiDeme <demelashnatnael@gmail.com>
2022-10-26 09:52:48 +0300

    Add delete tour feature to codebase

--- app/controllers/api/v1/tour_controller.rb
+++ app/controllers/api/v1/tour_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::TourController < ApplicationController
   def index
-    @tour = Tour.all
-    render json: @tour
+    @tours = Tour.all
+    render json: @tours
+  end
+  def destroy
+    @tour = Tour.find(params[:id])
+    @tour.destroy
+    render json: { success: true, message: 'Tour deleted successfully'}, status: :ok
   end
 end
