680435abec827fd1a36224a5c6181f5a42addf68
unknown <tylher123@gmail.com>
2022-10-31 12:09:01 +0100

    install and configure rswag for api documentation

---
+++ spec/requests/api/users_spec.rb
@@ -0,0 +1,30 @@
+require 'swagger_helper'
+
+describe 'Users API' do
+    path '/signup' do
+        post 'Creates a user' do
+            tags 'Users'
+            consumes 'application/json'
+            parameter name: :user, in: :body, schema: {
+                type: :object,
+                properties: {
+                    name: {type: :string},
+                    password: {type: :string},
+                    username: {type: :string},
+                    email: {type: :string}
+                },
+                required: ['name','email','password']
+            }
+            response '201', 'user created' do
+                let(:user) {{
+                    name: "name",
+                    password: "password",
+                    username: "test",
+                    email: "test@test.com"
+                  }}
+                  run_test!
+            end
+        end
+    end
+
+end
