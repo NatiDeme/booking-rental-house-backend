ae461cf81bc2988662bade39093735aca130e63a
NatiDeme <demelashnatnael@gmail.com>
2022-10-31 09:08:57 +0300

    Add create reservation feature

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -1,11 +1,11 @@
 class ReservationController < ApplicationController
     def create
-      @reservation = reservation.new(reserve_params)
+      @reservation = Reservation.new(reserve_params)
       @tour_id = params.permit(:tour_id)
       @tour = Tour.find_by(id: @tour_id['tour_id'])
-      @reservation.tour = @tour
-      @registration.user = current_user
-      if @registration.save
+      @reservation.tours = @tour
+      @reservation.user = current_user
+      if @reservation.save
       render json: @reservation, status: :created
     else
       render json: @reservation.error, status: :unprocessable_entity
