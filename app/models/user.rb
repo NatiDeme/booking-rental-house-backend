b49a3a36c44d1fe2315b9b50eadb2e162c50ad67
NatiDeme <demelashnatnael@gmail.com>
2022-11-02 20:10:14 +0300

    Add fix to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :tours, foreign_key: 'user_id'
-  has_many :reservations, foreign_key: 'user_id'
+  has_many :reservations, foreign_key: 'user_id', dependent: :delete_all
 
   validates :name, presence: true
   has_secure_password
