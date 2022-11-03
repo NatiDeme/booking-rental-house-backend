44a892e64060a3104ee583544ae2cd0e18ef4ba8
NatiDeme <demelashnatnael@gmail.com>
2022-11-03 03:19:30 +0300

    Add fix to rspec structure

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,7 +1,55 @@
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe 'Authentications', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+describe 'Authenticate API' do
+  path '/login' do
+    post 'Login a user' do
+      tags 'users'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :login_param, in: :body, schema: {
+        type: :object,
+        properties: {
+          username: { type: :string },
+          password: { type: :string }
+        },
+        required: %w[username password]
+      }
+
+      response '200', 'Login successful' do
+        schema type: :object,
+               properties: {
+                 token: { type: :string },
+                 user: {
+                   type: :object,
+                   properties: {
+                     name: { type: :string },
+                     user_id: { type: :integer }
+                   }
+                 }
+               }
+        user1 = User.create(
+          name: 'name',
+          password: 'password',
+          username: 'test',
+          email: 'test@test.com'
+        )
+        let(:login_param) do
+          {
+            username: user1.username,
+            password: user1.password
+          }
+        end
+        run_test!
+      end
+      response '401', 'Login failed' do
+        let(:login_param) do
+          {
+            username: '',
+            password: ''
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
